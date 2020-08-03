0951294b722948e152de3966be63fa1a2c00e898
T-Iwata0910 <t.iwata0910@gmail.com>
2020-08-03 14:21:00 +0900

    クラスのコンストラクタを変更 * 近似器の入力と出力変数数を必須引数に * 初期パラメータと初期共分散行列，忘却定数をオプション引数に

--- MultivariableRecursiveLS.m
+++ MultivariableRecursiveLS.m
@@ -3,22 +3,33 @@ classdef MultivariableRecursiveLS < handle
         ForgettingFactor (1, 1) double
         
     end
-%     properties(Access=private)
-    properties
+    properties(Access=private)
         Covariance
         Parameter
     end
     methods
-        function obj = MultivariableRecursiveLS(initialParameter, initialCovariance, forgettingFactor)
+        function obj = MultivariableRecursiveLS(inputSize, outputSize, options)
             arguments
-                initialParameter double
-                initialCovariance double
-                forgettingFactor (1, 1) double = 1
+                inputSize (1, 1) double {mustBePositive}
+                outputSize (1, 1) double {mustBePositive}
+                options.initialParameter double = double.empty
+                options.initialCovariance double = double.empty
+                options.ForgettingFactor (1, 1) double {mustBeNonnegative, mustBeLessThanOrEqual(options.ForgettingFactor,1)} = 1
             end
             
-            obj.Parameter = initialParameter;
-            obj.Covariance = initialCovariance;
-            obj.ForgettingFactor = forgettingFactor;
+            if isempty(options.initialParameter)
+                options.initialParameter = -0.05 + 0.1 * rand(inputSize, outputSize);
+            end
+            if isempty(options.initialCovariance)
+                options.initialCovariance = eye(inputSize) * 1e4;
+            end
+            
+            validateattributes(options.initialParameter, {'double'}, {'size', [inputSize, outputSize]}, 'initialParameter');
+            validateattributes(options.initialCovariance, {'double'}, {'size', [inputSize, inputSize]}, 'initialCovariance');
+            
+            obj.Parameter = options.initialParameter;
+            obj.Covariance = options.initialCovariance;
+            obj.ForgettingFactor = options.ForgettingFactor;
         end
     end
     methods
